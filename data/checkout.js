2f56886ea14f888c5e3f30bc332af0755e928e4c
pritam-30 <bhowmikpritam2016@gmail.com>
2024-08-12 19:18:57 +0530

    Update Files

--- data/checkout.js
+++ data/checkout.js
@@ -1,4 +1,19 @@
 import { renderCheckout } from "./orderSummary.js";
 import "./cart.js";
-import { loadProducts } from "./products.js";
-loadProducts(renderCheckout);
+import { loadProductsFetch } from "./products.js";
+/**
+ * A Promise-based function that loads products and resolves when done.
+ *
+ * @param {Function} callback-A callback function to be executed after products are loaded.
+ * @returns {Promise} A Promise that resolves when products are loaded.
+ *
+ * @example
+ */
+//TODO: Using Promise here for clear code and easily-readable.
+//TODO: Promise.all is used to run multiple given Promises
+//TODO: But the Promise.all below has only one Promise given inside it.
+// ! Resolve is used to go to the next time after loading the first given item.
+
+Promise.all([loadProductsFetch()]).then(() => {
+  renderCheckout();
+});
